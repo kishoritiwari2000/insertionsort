50a3e7efc6758d3d7865ae1cd68fc9cceeefbba2
kishoritiwari2000 <kishoritiwari2000@gmail.com>
2020-04-21 22:04:57 -0700

    this is the main class Main for implementing method insertionSortMethod

---
+++ src/main/Main.java
@@ -0,0 +1,22 @@
+package main;
+
+import insertionsortrecursion.InsertionSortClass;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("How many elements would you like in the array?: ");
+        final Scanner scanner = new Scanner(System.in);
+        int numberOfElements = scanner.nextInt();
+        int[] array = new int[numberOfElements];
+        //generating random numbers in the array
+        for (int i = 0; i < array.length; i++) {
+            array[i] = (int) (Math.random() * 100);
+        }
+        InsertionSortClass.insertionSortMethod(array);
+        System.out.println("ARRAY SORTED AFTER INSERTION SORT RECURSIVELY");
+        System.out.println(Arrays.toString(array));
+    }
+}
